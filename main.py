7e956196c78518f7e3a8399284fd2e563e574378
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-05-08 20:48:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,12 +23,21 @@ def disp(b,n,l,h,a,q):
     screen.blit(a, (0, 0))
     screen.blit(q, (0, 0))
     pg.display.update()
+count = 0
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             sys.exit(0)
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # Set the x, y postions of the mouse click
+            x, y = event.pos
+            if ng.get_rect().collidepoint(x, y):
+                print("hej")
     disp(bg,ng,lg,h,a,q)
     if pg.mouse.get_pressed()[2]:
+        if count == 0:
+            sys.exit(0)
+        count-=1
         ng = pg.image.load(os.path.join("images_title", "new_game.png")).convert_alpha()
         ng = pg.transform.scale(ng, (pg.display.Info().current_w, pg.display.Info().current_h))
         lg = pg.image.load(os.path.join("images_title", "load_game.png")).convert_alpha()
@@ -40,6 +49,7 @@ while True:
         q = pg.image.load(os.path.join("images_title", "quit.png")).convert_alpha()
         q = pg.transform.scale(q, (pg.display.Info().current_w, pg.display.Info().current_h))
     if pg.mouse.get_pressed()[0]:
+        count += 1
         ng = pg.image.load(os.path.join("images_title", "single_player.png")).convert_alpha()
         ng = pg.transform.scale(ng, (pg.display.Info().current_w, pg.display.Info().current_h))
         lg = pg.image.load(os.path.join("images_title", "multi_player.png")).convert_alpha()
