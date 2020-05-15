6732ef23b5a37a035a944ea387cb29e9a183ad16
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-05-15 20:23:10 +0200

    Update fight.py

--- fight.py
+++ fight.py
@@ -1,16 +1,30 @@
 import pygame as pg
 import sys
 import os
+import math
+class Field(object):
+    def __init__(self):
+        self.x=10
+        self.y=10*math.sqrt(10)
+        self.angle=30
 class Fight(object):
     def __init__(self,screen):
         pg.init()
         dis = pg.display.Info()
         screen = pg.display.set_mode((dis.current_w, dis.current_h), pg.FULLSCREEN)
-        bg = pg.transform.scale(pg.image.load(os.path.join("dessert", "dessert.png")).convert(),(dis.current_w, dis.current_h))
+        battlefield = pg.transform.scale(pg.image.load(os.path.join("dessert", "dessert_bf.png")).convert_alpha(),(dis.current_w, dis.current_h))
+        bg = pg.transform.scale(pg.image.load(os.path.join("dessert", "dessert_bg.png")).convert_alpha(),(dis.current_w, dis.current_h))
         sk = pg.image.load(os.path.join("skeleton_warrior", "skeleton_warrior.png")).convert_alpha()
+        bf = [[0 for x in range(12)] for y in range(15)]
+        mouse = pg.transform.scale(pg.image.load(os.path.join("images_title2", "mouse.png")).convert_alpha(),(int(dis.current_w / 120), int(dis.current_h / 40)))
+        pg.mouse.set_visible(False)
         while True:
-            screen.blit(bg, (0,0))
+            mx, my = pg.mouse.get_pos()
+            screen.blit(battlefield, (0,0))
+            screen.blit(bg, (0, 0))
             screen.blit(sk, (0, 0))
+            screen.blit(mouse,(mx,my))
+            pg.draw.polygon(screen, (0, 255, 0), ((100, 100), (100+10, 100+10), (100+20, 100-10),(100+20,100-20),(100+10,100)), 5)
             pg.display.update()
             for event in pg.event.get():
                 if event.type == pg.QUIT:
