483ebfe037e5bebd1bab9d28b67f42b6d2cc792a
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-05-20 19:46:47 +0200

    Update monsters.py

--- monsters.py
+++ monsters.py
@@ -1,9 +1,11 @@
-skeleton_warrior={
-    "att" : 2,
-    "def" : 3,
-    "min" : 2,
-    "max" : 3,
-    "hp" : 10,
-    "arr" : 0,
-    "spd" : 5
+monsters ={
+    "skeleton_warrior" : {
+        "att" : 2,
+        "def" : 3,
+        "min" : 2,
+        "max" : 3,
+        "hp" : 10,
+        "arr" : 0,
+        "spd" : 5
+    }
 }
