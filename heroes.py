6890664c4ffa3a1af397302a1a3511754e2a50aa
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-05-21 20:21:25 +0200

    Add files via upload

---
+++ heroes.py
@@ -0,0 +1,16 @@
+from monsters import monsters
+heroes={
+    "Sandro" :{
+        "ATT" : 0,
+        "DEF" : 0,
+        "KN" : 0,
+        "MG" : 0,
+        "slot1" : monsters["skeleton_warrior"],
+        "slot2" : monsters["skeleton_warrior"],
+        "slot3" : monsters["skeleton_warrior"],
+        "slot4" : monsters["skeleton_warrior"],
+        "slot5" : monsters["skeleton_warrior"],
+        "slot6" : monsters["skeleton_warrior"],
+        "slot7" : monsters["skeleton_warrior"],
+        }
+}
