9be212828ae84cb71291a22faf44713216323b04
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-08-07 21:21:40 +0200

    Update heroes.py

--- heroes.py
+++ heroes.py
@@ -6,13 +6,8 @@ heroes={
         "KN" : 0,
         "MG" : 0,
         "slot" : {
-            "1" : monsters["skeleton_warrior"],
-            "2" : monsters["skeleton_warrior"],
-            "3" : monsters["skeleton_warrior"],
-            "4" : monsters["skeleton_warrior"],
-            "5" : monsters["skeleton_warrior"],
-            "6" : monsters["skeleton_warrior"],
-            "7" : monsters["skeleton_warrior"],
+            "1" : [monsters["storm_bird"],1],
+            "2" : [monsters["skeleton_warrior"],1]
         }
     }
 }
